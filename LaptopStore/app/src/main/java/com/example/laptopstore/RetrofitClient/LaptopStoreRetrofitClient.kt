3bdb8b4b1358e5c0c0b4c3ceba4c62555e3907ee
Oanh1503-git <132774263+Oanh1503-git@users.noreply.github.com>
2025-05-11 21:58:20 +0700

    api khachhang

---
+++ LaptopStore/app/src/main/java/com/example/laptopstore/RetrofitClient/LaptopStoreRetrofitClient.kt
@@ -0,0 +1,5 @@
+package com.example.laptopstore.RetrofitClient
+
+object Constants {
+    const val BASE_URL = "http://10.0.2.2/lap_store_api/api/"
+}
---
+++ LaptopStore/app/src/main/java/com/example/laptopstore/api/KhachHangAPIService.kt
@@ -0,0 +1,2 @@
+package com.example.laptopstore.api
+
