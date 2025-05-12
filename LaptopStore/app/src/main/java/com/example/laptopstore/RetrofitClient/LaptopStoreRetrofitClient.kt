00860e8440d76430fe010fb917c72e04d8d61e60
Oanh1503-git <132774263+Oanh1503-git@users.noreply.github.com>
2025-05-12 22:13:27 +0700

    chages khachhangApi

--- LaptopStore/app/src/main/java/com/example/laptopstore/RetrofitClient/LaptopStoreRetrofitClient.kt
+++ LaptopStore/app/src/main/java/com/example/laptopstore/RetrofitClient/LaptopStoreRetrofitClient.kt
@@ -1,67 +1,59 @@
 package com.example.laptopstore.RetrofitClient
 
 import ChiTietHoaDonBanAPIService
+import DiaChiAPIService
+import GioHangAPIService
 import HinhAnhAPIService
+import HoaDonBanAPIService
 import SanPhamAPIService
 import TaiKhoanAPIService
-import com.google.android.gms.common.internal.Constants
+import com.example.laptopstore.api.KhachHangAPIService
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Constants {
+    const val BASE_URL = "http://10.0.2.2/ten_du_an_api/"  // hoặc địa chỉ XAMPP của bạn
+}
 
 object QuanLyBanLaptopRetrofitClient {
-    val sanphamAPIService: SanPhamAPIService by lazy {
+
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .build()
-            .create(SanPhamAPIService::class.java)
+    }
+
+    val sanphamAPIService: SanPhamAPIService by lazy {
+        retrofit.create(SanPhamAPIService::class.java)
     }
 
     val hinhAnhAPIService: HinhAnhAPIService by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(HinhAnhAPIService::class.java)
+        retrofit.create(HinhAnhAPIService::class.java)
     }
+
     val taiKhoanAPIService: TaiKhoanAPIService by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(TaiKhoanAPIService::class.java)
+        retrofit.create(TaiKhoanAPIService::class.java)
     }
 
-    val khachHangAPIService: KhachHangAPIService = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-        .build()
-        .create(KhachHangAPIService::class.java)
+    val khachHangAPIService: KhachHangAPIService by lazy {
+        retrofit.create(KhachHangAPIService::class.java)
+    }
 
-    val giohangAPIService: GioHangAPIService = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-        .build()
-        .create(GioHangAPIService::class.java)
+    val giohangAPIService: GioHangAPIService by lazy {
+        retrofit.create(GioHangAPIService::class.java)
+    }
 
     val hoaDonBanAPIService: HoaDonBanAPIService by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(HoaDonBanAPIService::class.java)
+        retrofit.create(HoaDonBanAPIService::class.java)
     }
 
     val diaChiAPIService: DiaChiAPIService by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(DiaChiAPIService::class.java)
+        retrofit.create(DiaChiAPIService::class.java)
     }
 
     val chiTietHoaDonBanAPIService: ChiTietHoaDonBanAPIService by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(ChiTietHoaDonBanAPIService::class.java)
+        retrofit.create(ChiTietHoaDonBanAPIService::class.java)
     }
+}
